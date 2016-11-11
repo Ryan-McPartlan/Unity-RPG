0c3f4dff7cb897f756af410124284cd4386fa8f5
skelly1324 <ryanmcp45@gmail.com>
2016-11-11 17:43:41 -0500

    Create WeaponController.cs

---
+++ Managers/WeaponController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+    public static ComponentItem[] GetDefaultComponents(string weaponSlug)
+    {
+        ComponentItem[] components = new ComponentItem[3];
+
+        return components;
+    }
+}
